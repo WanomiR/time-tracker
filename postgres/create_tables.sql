4c3830fd2e5bf94c8e8d0cc75e2208ba35e2db82
Wanomir <ioan.romadin@gmail.com>
2024-06-29 08:01:11 +0300

    feat: database structure, create tables with mockup entries

--- postgres/create_tables.sql
+++ postgres/create_tables.sql
@@ -3,16 +3,43 @@ CREATE TABLE public.users
     id              SERIAL PRIMARY KEY,
     passport_series INTEGER,
     passport_number INTEGER,
-    surname         VARCHAR(30) NOT NULL,
-    name            VARCHAR(30) NOT NULL,
+    surname         VARCHAR(30)  NOT NULL,
+    name            VARCHAR(30)  NOT NULL,
     patronymic      VARCHAR(30),
-    address         VARCHAR(250)
+    address         VARCHAR(250) NOT NULL
 );
 
-INSERT INTO public.users (passport_series, passport_number, surname, name, patronymic, address) VALUES
-    (1234, 567890, 'Ivanov', 'Ivan', 'Ivanovich', 'Moscow, Lenina 20 apt. 321'),
-    (1534, 157798, 'Sherlock', 'Holmes', NULL, 'London, 221B Backer Street'),
-    (2626, 125616, 'Jack', 'Reacher', NULL, NULL),
-    (9876, 829769, 'Pavlov', 'Konstantin', 'Andreevich', 'Nizhniy Novgorod, Stroiteley 12'),
-    (3265, 290523, 'Verstappen', 'Max', NULL, 'Monaco, 76 Crosby street')
+INSERT INTO public.users (passport_series, passport_number, surname, name, patronymic, address)
+VALUES (1234, 567890, 'Ivanov', 'Ivan', 'Ivanovich', 'Moscow, Lenina 5 apt. 1'),
+       (1534, 157798, 'Sherlock', 'Holmes', NULL, 'London, 221B Backer Street'),
+       (2626, 125616, 'Jack', 'Reacher', NULL, 'Undefined'),
+       (9876, 829769, 'Pavlov', 'Konstantin', 'Andreevich', 'Nizhniy Novgorod, Stroiteley 12'),
+       (3265, 290523, 'Verstappen', 'Max', NULL, 'Monaco, 76 Crosby street')
+;
+
+CREATE TABLE public.tasks
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER      NOT NULL,
+    task        VARCHAR(100) NOT NULL,
+    started_at  timestamp    NOT NULL,
+    finished_at timestamp,
+    duration    INTEGER
+);
+
+INSERT INTO public.tasks (user_id, task, started_at, finished_at, duration)
+VALUES (1, 'дойка коровы', (now() - INTERVAL '1155 seconds'), now(), 1155),
+       (1, 'прополка грядкок', (now() - INTERVAL '10532 seconds'), now(), 10523),
+       (1, 'чистка амбара', (now() - INTERVAL '5116 seconds'), now(), 5116),
+       (5, 'RB20 tests', (now() - INTERVAL '7626 seconds'), now(), 7626),
+       (5, 'sprint qualifying', (now() - INTERVAL '3161 seconds'), now(), 3161),
+       (5, 'qualifying', (now() - INTERVAL '4915 seconds'), now(), 4915),
+       (5, 'Austrian Grand Prix', (now() - INTERVAL '5166 seconds'), now(), 5166),
+       (3, 'murder investigation', (now() - INTERVAL '7235 seconds'), now(), 7235),
+       (3, 'fighting in the bar', (now() - INTERVAL '1800 seconds'), now(), 1800),
+       (2, 'arguing with Watson', (now() - INTERVAL '4531 seconds'), now(), 4531),
+       (2, 'investigating', (now() - INTERVAL '5634 seconds'), now(), 5634),
+       (4, 'groceries', (now() - INTERVAL '3235 seconds'), now(), 3235),
+       (4, 'plumbing', (now() - INTERVAL '2662 seconds'), now(), 2662),
+       (4, 'fixing the porch', (now() - INTERVAL '5432 seconds'), now(), 5432)
 ;
