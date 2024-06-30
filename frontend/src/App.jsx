7af03f91db8f114abd97a6b1205bf320771639e4
Wanomir <ioan.romadin@gmail.com>
2024-06-30 13:33:56 +0300

    feat: setup frontend structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+	return (
+		<div className="container">
+			<div className="col">
+				<h1 className="mt-3">TimeTracker</h1>
+			</div>
+			<hr className="mb-3"/>
+		</div>
+	)
+}
+
+export default App
