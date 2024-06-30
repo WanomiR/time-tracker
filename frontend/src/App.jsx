bacbb003e71f03d14c7b28e94aa53694725d1d3e
Wanomir <ioan.romadin@gmail.com>
2024-06-30 13:56:54 +0300

    feat: basic router and error component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
+import {Link, Outlet} from "react-router-dom";
 
 
 const App = () => {
 	return (
 		<div className="container">
-			<div className="col">
-				<h1 className="mt-3">TimeTracker</h1>
+			<div className="row">
+				<div className="col">
+					<h1 className="mt-3">TimeTracker</h1>
+				</div>
+				<hr className="mb-3"/>
 			</div>
-			<hr className="mb-3"/>
+
+			<div className="row">
+				<div className="col-md-2">
+					<nav>
+						<div className="list-group">
+							<Link to={"/"} className={"list-group-item list-group-item-action"}>Timer</Link>
+							<Link to={"/tasks"} className={"list-group-item list-group-item-action"}>My Tasks</Link>
+							<Link to={"/users"} className={"list-group-item list-group-item-action"}>Users</Link>
+						</div>
+					</nav>
+				</div>
+				<div className="col-md-10">
+					<Outlet />
+				</div>
+			</div>
 		</div>
 	)
 }
